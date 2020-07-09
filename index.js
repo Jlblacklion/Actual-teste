3ab3d041f0cacdfd20f9dba6d354c883f0300c67
joel <joel.ad.manuel@gmail.com>
2020-07-08 23:59:20 -0300

    Adicionado o modulo de aarquivos estaticos

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pagamento = require("./models/Formulariodb")
+var   path = require('path');
 
 //Extra 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(express.static(path.join(__dirname,"/landing-page/")));
+
 // Rotas 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/landing-page/Formulario.html");
+  res.sendFile(__dirname + "/landing-page/formulario.html");
 });
 
-app.post('/form', function(req, res){
+
+app.post('/', function(req, res){
   pagamento.create({
-      nome: req.body.nome,
+      nome:            req.body.nome,
       data_nascimento: req.body.data_nascimento,
-      email:req.body.email,
-      telefone: req.body.telefone
+      email:           req.body.email,
+      telefone:        req.body.telefone
   }).then(function(){
-      res.sendFile('/formulario')
+      res.redirect('#####');
   }).catch(function(erro){
-      res.send("Erro: Pagamento não foi cadastrado!" + erro)
-  })
-})
- 
+      res.send("Erro: Pagamento não foi cadastrado!" + erro);
+  });
+});
+
+app.post('/form', function (req, res) {
+  res.send("formulario");
+});
+
 app.listen(3000);
